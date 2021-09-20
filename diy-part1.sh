d3bcc333b94e3ccd835fcc23448664f55f2e6841
lxl6125 <lxl6125@yeah.net>
2021-09-21 00:48:43 +0800

    Add device: Raspberry Pi 4B

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,29 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Rename hostname to OpenWrt
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# enable wifi
+sed -i 's/VHT80/VHT20/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Add Firmware release link
+sed -i "s#ImmortalWrt Core</a>#ImmortalWrt Core</a> / <a href="https://github.com/lxl6125/ImmortalWrt_Raspberry-Pi-4B/releases" target="_blank">Firmware release</a>#g" package/emortal/autocore/files/arm/index.htm
+
+# Add build date to index page
+sed -i '/exit 0/d' package/emortal/default-settings/files/zzz-default-settings
+cat >> package/emortal/default-settings/files/zzz-default-settings << "EOF"
+export orig_revision="$(cat "/etc/openwrt_release" | grep DISTRIB_REVISION | awk -F "'" '{print $2}')"
+sed -i "s/${orig_revision}/build_date/" /etc/openwrt_release
+
+exit 0
+EOF
+sed -i "s/build_date/($(date +"%Y-%m-%d"))/" package/emortal/default-settings/files/zzz-default-settings
+
+# Use kernel version 5.10
+sed -i "s/5.4/5.10/g" target/linux/bcm27xx/Makefile
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,24 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# TTYD automatic login
+#sed -i 's#/bin/login#/usr/libexec/login.sh#g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Replace qBittorrent-Enhanced-Edition
+rm -rf feeds/packages/net/qBittorrent-Enhanced-Edition/patches
+cp -f $GITHUB_WORKSPACE/qBittorrent-Enhanced-Edition feeds/packages/net/qBittorrent-Enhanced-Edition/Makefile
+
+# Replace luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash feeds/luci/applications/luci-app-openclash
+$GITHUB_WORKSPACE/preset-clash-core.sh armv8
+
+# Replace luci-app-dockerman
+rm -rf feeds/luci/applications/luci-app-dockerman
+svn co https://github.com/lxl6125/luci-app-dockerman/trunk/applications/luci-app-dockerman feeds/luci/applications/luci-app-dockerman
+sed -i '127,131s/#//' feeds/packages/utils/dockerd/Makefile
+
+# Replace luci-app-unblockneteasemusic
+rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
+git clone --depth=1 https://github.com/immortalwrt/luci-app-unblockneteasemusic.git feeds/luci/applications/luci-app-unblockneteasemusic
+
